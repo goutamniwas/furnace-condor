3ea0542128ea354ffb803717a12da4780f59c4ad
savoca <adeddo27@gmail.com>
2014-11-12 10:46:56 +0000

    Furnace-1.1.0

--- export.sh
+++ export.sh
@@ -14,7 +14,7 @@ else
 	export CROSS_COMPILE=/home/savoca/storage/toolchains/arm-eabi-4.7/bin/arm-eabi-
 fi
 kernel="furnace"
-version="1.0.6"
+version="1.1.0"
 rom="stock"
 variant="condor"
 config="furnace_defconfig"
