c7754825d85236e827b29a4d6c742be0f27b6c60
savoca <adeddo27@gmail.com>
2014-11-19 14:44:00 -0500

    ozip: Show information for condor  - Also use condor offsets in export.sh

--- export.sh
+++ export.sh
@@ -24,7 +24,7 @@ ps=2048
 base_offset=0x00000000
 ramdisk_offset=0x01000000
 tags_offset=0x00000100
-cmdline="console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 vmalloc=400M utags.blkdev=/dev/block/platform/msm_sdcc.1/by-name/utags"
+cmdline="console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 utags.blkdev=/dev/block/platform/msm_sdcc.1/by-name/utags vmalloc=400M"
 
 function cleanme {
 	if [ -f arch/arm/boot/"$kerneltype" ]; then
